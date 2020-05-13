2c5f40008a9e430057ba5f890e10b4844a23b879
Aleksander Bondyuk <aleksander.bondyuk@yandex.ru>
2020-05-13 20:43:17 +0700

    init project

---
+++ src/test/java/ru/bondyuk/calllist/CallListApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bondyuk.calllist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
